4ecfad22981e86671ee136c3522e2379f2deba3b
Tumhara Naam <tumharaemail@example.com>
2025-03-28 23:12:19 +0500

    Readable code of my text field

--- bmi_flutter_app/lib/widgets/my_text_field.dart
+++ bmi_flutter_app/lib/widgets/my_text_field.dart
@@ -24,26 +24,52 @@ class MyTextField extends StatelessWidget {
       child: SizedBox(
         width: 400,
         child: TextField(
+          // Text Field Controller
           controller: myController,
+
+          // Text Field Cursor Color
           cursorColor: Colors.blue,
+
+          // Text Field Cursor height
           cursorHeight: 22,
+
+          // Text Field Key board input type
           keyboardType: TextInputType.numberWithOptions(),
+
+          // Text Field Decoration
           decoration: InputDecoration(
+            // Padding between contents
             contentPadding: EdgeInsets.all(18),
+
+            // Label Behavior always on top of the text field
             floatingLabelBehavior: FloatingLabelBehavior.always,
+
+            // Label Text
             label: Text(labelText),
+
+            // Label Text Style
             labelStyle: TextStyle(fontSize: 14, color: Colors.blue),
+
+            // Prefix Icon which is on left side of the text field
             prefixIcon: icon,
+
+            // Hint Text
             hintText: hintText,
+
+            // Hint Text Style
             hintStyle: TextStyle(
               color: Colors.grey,
               fontSize: 14,
-              fontWeight: FontWeight.w200,
+              fontWeight: FontWeight.w300,
             ),
+
+            // Enabled Border
             enabledBorder: OutlineInputBorder(
               borderSide: BorderSide(color: Colors.grey),
               borderRadius: BorderRadius.circular(12),
             ),
+
+            // Focused Border
             focusedBorder: OutlineInputBorder(
               borderSide: BorderSide(color: Colors.blue),
               borderRadius: BorderRadius.circular(12),
